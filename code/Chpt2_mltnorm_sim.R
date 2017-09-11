99766e3628e9cb76c245073caf5587fa43afbd14
Alexander Durbin <ajd2@vt.edu>
2017-09-11 11:22:08 -0400

    ALEX: Modify regression procedure. Was not computing boundary correctly

--- code/Chpt2_mltnorm_sim.R
+++ code/Chpt2_mltnorm_sim.R
@@ -13,30 +13,28 @@ set.seed(12345)
 mu <- c(1, 0)
 sigma <- diag(1, nrow = 2)
 center <- mvrnorm(n = 5, mu = mu, Sigma = sigma)
-centroids <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(1, 5))
+centroids <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(0, 5))
 
 
 mu <- c(0, 1)
 center <- mvrnorm(n = 5, mu = mu, Sigma = sigma)
-tmp <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(2, 5))
+tmp <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(1, 5))
 
 centroids <- bind_rows(centroids, tmp)
-
-# garbage collection
-rm(mu, center, tmp)
 
 # generate data
 
 my_data <- tibble(x1 = rep(0, 100), x2 = rep(0, 100), class = rep(NA, 100),
                   subclass = rep(NA, 100))
 
+# make this a function with apply statement later
 for (i in 1:nrow(my_data)) {
   
   row_index <- sample(1:10, 1)
   
   new_point <- mvrnorm(n = 1,
                        mu = c(centroids$x1[row_index], centroids$x2[row_index]),
-                       Sigma = sigma) 
+                       Sigma = sigma / 5) 
   
   my_data$x1[i] = new_point[1]
   my_data$x2[i] = new_point[2]
@@ -45,11 +43,24 @@ for (i in 1:nrow(my_data)) {
   
 }
 
+# make class and subclass a factor for plotting
+# centroids$class = as.factor(centroids$class)
+# my_data$class = as.factor(my_data$class)
+# my_data$subclass = as.factor(my_data$subclass)
 
 # plot data by class
-ggplot(data = my_data) +
-  geom_point(mapping = aes(x = x1, y = x2, color = factor(class)))
+# ggplot(data = my_data) +
+#   geom_point(mapping = aes(x = x1, y = x2, color = class)) +
+#   stat_smooth(mapping = aes(x = x1, y = x2), method = 'lm', se = FALSE,
+#               fullrange = TRUE)
 
-# plot data by subclass
-ggplot(data = my_data) +
-  geom_point(mapping = aes(x = x1, y = x2, color = factor(subclass)))
+
+
+# linear model using x1, x2 to predict 0,1 response
+lmfit <- lm(class ~ x1 + x2, data = my_data)
+
+tmp <- my_data %>% 
+  dplyr::select(x1, x2) %>% 
+  mutate(intercept = rep(1, nrow(my_data))) %>% 
+  dplyr::select(intercept, x1, x2)
+
