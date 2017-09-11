b199ee33bf9b41123d2684f8eeba12906242085a
Alexander Durbin <ajd2@vt.edu>
2017-09-11 09:50:48 -0400

    initial data generated

--- code/Chpt2_mltnorm_sim.R
+++ code/Chpt2_mltnorm_sim.R
@@ -1,10 +1,45 @@
 # Chapter 2 Normal Mixture Models Simulation
 # Alexander Durbin
+# Worry about optimization later
+
+rm(list = ls())
 
 library(tidyverse)
 library(MASS)
 
-# first generate 10 centroids
-mu <- c(0,0)
+set.seed(12345)
+
+# generate centroids
+mu <- c(1, 0)
 sigma <- diag(1, nrow = 2)
-centers <- mvrnorm(10, mu = mu, Sigma = sigma)
+center <- mvrnorm(n = 5, mu = mu, Sigma = sigma)
+centroids <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(1, 5))
+
+
+mu <- c(0, 1)
+center <- mvrnorm(n = 5, mu = mu, Sigma = sigma)
+tmp <- tibble(x1 = center[, 1], x2 = center[, 2], class = rep(2, 5))
+
+centroids <- bind_rows(centroids, tmp)
+
+# generate data
+
+my_data <- tibble(x1 = rep(0, 100), x2 = rep(0, 100), class = rep(NA, 100),
+                  subclass = rep(NA, 100))
+
+# garbage collection
+rm(mu, center, tmp)
+
+for (i in 1:nrow(my_data)) {
+  
+  row_index <- sample(1:10, 1)
+  
+  new_point <- mvrnorm(n = 1, mu = c(centroids$x1[row_index], centroids$x2[row_index]),
+                       Sigma = sigma) 
+  
+  my_data$x1[i] = new_point[1]
+  my_data$x2[i] = new_point[2]
+  my_data$class[i] = centroids$class[row_index]
+  my_data$subclass[i] = row_index
+  
+}
