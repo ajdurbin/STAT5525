3b2f9c6d628d8031f4a6322b3b7ec3e2ebdb5eb3
Alexander Durbin <ajd2@vt.edu>
2017-10-08 13:50:42 -0400

    concattenated role strings in case users have multple roles, then ill be able to look at that string and separate roles by space

--- project/explore/combine.R
+++ project/explore/combine.R
@@ -27,6 +27,7 @@ emp <- list.files(path = "~/data_analytics/DataSets1_9182017/Employees_info/",
                   pattern = "*.csv", full.names = TRUE)
 emp <- do.call(rbind, lapply(emp, read_csv)) %>% 
   mutate(user = stringr::str_c(stringr::str_sub(Domain, 1, 4), user_id, sep = "/"))
+emp$Role <- stringr::str_replace_all(emp$Role, " ", "")
 
 # balance between social media, streaming, shopping, job searching
 bad_sites <- c("facebook", "myspace", "twitter", "instagram", "pinterest",
