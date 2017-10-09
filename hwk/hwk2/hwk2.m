5b08743f6bb0c9f4da430b743e8d6e75487ccc1f
Alexander Durbin <ajd2@vt.edu>
2017-10-09 13:31:11 -0400

    added some comments. there is something definitely wrong because the tmin only has 3 nodes

--- hwk/hwk2/hwk2.m
+++ hwk/hwk2/hwk2.m
@@ -3,6 +3,7 @@ load spam.data.txt
 X = spam_data(:, 1:57);
 Y = spam_data(:, 58);
 
+%% single run
 % classification tree
 t = classregtree(X, Y, 'method', 'classification');
 % view(t);
@@ -13,22 +14,26 @@ PredVals = eval(t,X);
 PredVals = str2num(cell2mat(PredVals));
 pct = mean(abs(PredVals - Y));
 
+%% all together
 % now have this working some, time to cross validate it
 folds = 10;
 indices = cvpartition(length(Y), 'k', folds);
 err = zeros(10, 1);
 opterr = zeros(10, 1);
 for i = 1:10
+    % get training and testing
     testset = test(indices,i); trainset = ~testset;
     Ytest = Y(testset); Xtest = X(testset);
     Ytrain = Y(trainset); Xtrain = X(trainset);
     t = classregtree(Xtrain, Ytrain, 'method', 'classification');
     % prune for best tree
+    
     [c,s,n,best] = test(t, 'cross', Xtrain, Ytrain);
     tmin = prune(t, 'level', best);
     optPredVals = eval(tmin, Xtest);
     optPredVals = str2num(cell2mat(optPredVals));
     opterr(i) = mean(abs(optPredVals - Ytest));
+    
     % no pruning
     PredVals = eval(t, Xtest);
     PredVals = str2num(cell2mat(PredVals));
