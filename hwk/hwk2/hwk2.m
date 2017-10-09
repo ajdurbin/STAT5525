f0cab57b0181fe6aed92af9b19ba8b4cdb76586c
Alexander Durbin <ajd2@vt.edu>
2017-10-09 13:26:32 -0400

    added cross validation and optimal

--- hwk/hwk2/hwk2.m
+++ hwk/hwk2/hwk2.m
@@ -5,10 +5,32 @@ Y = spam_data(:, 58);
 
 % classification tree
 t = classregtree(X, Y, 'method', 'classification');
-view(t)
+% view(t);
 % total nodes in tree
-numnodes(t)
+numnodes(t);
 
 PredVals = eval(t,X);
 PredVals = str2num(cell2mat(PredVals));
 pct = mean(abs(PredVals - Y));
+
+% now have this working some, time to cross validate it
+folds = 10;
+indices = cvpartition(length(Y), 'k', folds);
+err = zeros(10, 1);
+opterr = zeros(10, 1);
+for i = 1:10
+    testset = test(indices,i); trainset = ~testset;
+    Ytest = Y(testset); Xtest = X(testset);
+    Ytrain = Y(trainset); Xtrain = X(trainset);
+    t = classregtree(Xtrain, Ytrain, 'method', 'classification');
+    % prune for best tree
+    [c,s,n,best] = test(t, 'cross', Xtrain, Ytrain);
+    tmin = prune(t, 'level', best);
+    optPredVals = eval(tmin, Xtest);
+    optPredVals = str2num(cell2mat(optPredVals));
+    opterr(i) = mean(abs(optPredVals - Ytest));
+    % no pruning
+    PredVals = eval(t, Xtest);
+    PredVals = str2num(cell2mat(PredVals));
+    err(i) = mean(abs(PredVals - Ytest));
+end
