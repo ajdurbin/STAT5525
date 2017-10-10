3d4d67928e7af51d9290c2c82bef3dc635529f1a
Alexander Durbin <ajd2@vt.edu>
2017-10-10 15:38:54 -0400

    updated R code for cart algorith, error rates smaller but still high. there is improvement after pruning but maybe not as much as there should be. no overfitting problem here either

--- hwk/hwk2/rpart.R
+++ hwk/hwk2/rpart.R
@@ -6,7 +6,7 @@ raw[, 1 : 57] <- scale(raw[, 1 : 57])
 raw[, 58] <- as.factor(raw[, 58])
 
 carfit <- rpart(V58 ~ ., data = raw, method = "class",
-                control = list(cp = 0.0001, minsplit = 2, minbucket = 1))
+                control = list(cp = .Machine$double.eps, minsplit = 2, minbucket = 1))
 printcp(carfit)
 plotcp(carfit)
 # see that we have 286 nodes in this by this specification
@@ -15,6 +15,46 @@ plotcp(carfit)
 # from the plotcp
 
 carpred <- predict(carfit, newdata = raw[, 1: 57], type="class")
-table(raw[, 58], carpred)
+conf <- table(raw[, 58], carpred)
 # error is then
-5 / nrow(raw)
+cler <- (conf[2]  + conf[3]) / (conf[1] + conf[4])
+
+
+
+# loop to do cross validation ---------------------------------------------
+
+
+# now wrap up in a function
+# error storage
+preders <- rep(0, 10)
+# split into training and testing
+train_index <- createDataPartition(y = raw[, 58], times = 10, p = 0.8, list = FALSE)
+
+for(i in 1 : 10){
+  
+  # train/test sets
+  raw_train <- raw[train_index[, i], ]
+  raw_test <- raw[-train_index[, i], ]
+  
+  # initial fit
+  carfit <- rpart(V58 ~ ., data = raw_train, method = "class",
+                  control = list(cp = .Machine$double.eps, minsplit = 2, 
+                                 minbucket = 1))
+  # find best cp value 
+  bestcp <- carfit$cptable[which.min(carfit$cptable[,"xerror"]),"CP"]
+  
+  # prune tree 
+  carfit_prune <- prune(carfit, cp = bestcp)
+  
+  # make predictions
+  carpred <- predict(carfit_prune, newdata = raw_test[, 1:57], type="class")
+  
+  # confusion matrix and error rate
+  conf <- table(raw_test[, 58], carpred)
+  preders[i] <- (conf[2]  + conf[3]) / (conf[1] + conf[4])
+  
+}
+
+paste0("The overall classification error is: ", mean(preders))
+
+
