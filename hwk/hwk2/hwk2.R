053213a8ab2e75f79b3506a17e9bdd2a35a3f39d
Alexander Durbin <ajd2@vt.edu>
2017-10-11 11:57:51 -0400

    finished cart problem and added random forest component

--- hwk/hwk2/hwk2.R
+++ hwk/hwk2/hwk2.R
@@ -11,7 +11,6 @@ library(rpart)
 
 ## ------------------------------------------------------------------------
 
-set.seed(130)
 # load spam data set
 raw <- read.table("spam.data.txt")
 # standardize columns
@@ -22,13 +21,68 @@ raw[, 58] <- as.factor(raw[, 58])
 
 ## ------------------------------------------------------------------------
 
-trc <- trainControl(method = "cv", number = 10)
-carfit <- train(V58 ~ ., data = raw, trControl = trc, method = "rpart", 
-                control = list(minsplit = 2, cp = 10, maxcompete = 1000,
-                               minbucket = 1))
-carfit
-carfit$finalModel
-carfit$resample
+# error storage
+preders <- rep(0, 10)
+# split into training and testing
+train_index <- createDataPartition(y = raw[, 58], times = 10, p = 0.9, list = FALSE)
+
+for(i in 1 : 10){
+  
+  # train/test sets
+  raw_train <- raw[train_index[, i], ]
+  raw_test <- raw[-train_index[, i], ]
+  
+  # initial fit
+  carfit <- rpart(V58 ~ ., data = raw_train, method = "class",
+                  control = list(cp = .Machine$double.eps, minsplit = 2, 
+                                 minbucket = 1))
+  
+  # make predictions
+  carpred <- predict(carfit, newdata = raw_test[, 1:57], type="class")
+  # confusion matrix and error rate
+  
+  conf <- table(raw_test[, 58], carpred)
+  preders[i] <- (conf[2]  + conf[3]) / (conf[1] + conf[4])
+  
+}
+
+paste0("The overall classification error is: ", mean(preders))
+
+
+## ------------------------------------------------------------------------
+
+# error storage
+preders <- rep(0, 10)
+# split into training and testing
+train_index <- createDataPartition(y = raw[, 58], times = 10, p = 0.9, list = FALSE)
+
+for(i in 1 : 10){
+  
+  # train/test sets
+  raw_train <- raw[train_index[, i], ]
+  raw_test <- raw[-train_index[, i], ]
+  
+  # initial fit
+  carfit <- rpart(V58 ~ ., data = raw_train, method = "class",
+                  control = list(cp = .Machine$double.eps, minsplit = 2, 
+                                 minbucket = 1))
+  # find best cp value 
+  bestcp <- carfit$cptable[which.min(carfit$cptable[,"xerror"]),"CP"]
+  
+  # prune tree 
+  carfit_prune <- prune(carfit, cp = bestcp)
+  
+  # make predictions
+  carpred <- predict(carfit_prune, newdata = raw_test[, 1:57], type="class")
+  
+  # confusion matrix and error rate
+  conf <- table(raw_test[, 58], carpred)
+  preders[i] <- (conf[2]  + conf[3]) / (conf[1] + conf[4])
+  
+}
+
+paste0("The overall classification error is: ", mean(preders))
+
 
 ## ---- warning=FALSE------------------------------------------------------
 
@@ -45,7 +99,7 @@ logfit$resample
 
 ## ------------------------------------------------------------------------
 
-1 - logfit$results[, 2]
+paste0("Overall error rate: ", 1 - logfit$results[, 2])
 
 
 ## ------------------------------------------------------------------------
--- hwk/hwk2/hwk2.m
+++ hwk/hwk2/hwk2.m
@@ -14,8 +14,31 @@ PredVals = eval(t,X);
 PredVals = str2num(cell2mat(PredVals));
 pct = mean(abs(PredVals - Y));
 
+folds = 10;
+indices = cvpartition(length(Y), 'k', folds);
+testset = test(indices,1); trainset = ~testset;
+Ytest = Y(testset); Xtest = X(testset);
+Ytrain = Y(trainset); Xtrain = X(trainset);
+t = classregtree(Xtrain, Ytrain, 'method', 'classification');
+% prune for best tree
+
+[c,s,n,best] = test(t, 'test', Xtrain, Ytrain);
+tmin = prune(t, 'level', best);
+optPredVals = eval(tmin, Xtest);
+optPredVals = str2num(cell2mat(optPredVals));
+opterr = mean(abs(optPredVals - Ytest));
+
+% no pruning
+PredVals = eval(t, Xtest);
+PredVals = str2num(cell2mat(PredVals));
+err = mean(abs(PredVals - Ytest));
+
+
 %% all together
 % now have this working some, time to cross validate it
+load spam.data.txt
+X = spam_data(:, 1:57);
+Y = spam_data(:, 58);
 folds = 10;
 indices = cvpartition(length(Y), 'k', folds);
 err = zeros(10, 1);
@@ -28,7 +51,7 @@ for i = 1:10
     t = classregtree(Xtrain, Ytrain, 'method', 'classification');
     % prune for best tree
     
-    [c,s,n,best] = test(t, 'cross', Xtrain, Ytrain);
+    [c,s,n,best] = test(t, 'test', Xtrain, Ytrain);
     tmin = prune(t, 'level', best);
     optPredVals = eval(tmin, Xtest);
     optPredVals = str2num(cell2mat(optPredVals));
@@ -39,3 +62,5 @@ for i = 1:10
     PredVals = str2num(cell2mat(PredVals));
     err(i) = mean(abs(PredVals - Ytest));
 end
+mean(err)
+mean(opterr)
