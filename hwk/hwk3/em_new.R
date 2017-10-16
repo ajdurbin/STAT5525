b2d4986365927b47f4df4abd642fd3684d1aa1c2
Alexander Durbin <ajd2@vt.edu>
2017-10-16 16:07:32 -0400

    added comments

--- hwk/hwk3/em_new.R
+++ hwk/hwk3/em_new.R
@@ -1,6 +1,8 @@
 library(MASS)
 library(mvtnorm)
 
+rm(list = ls())
+
 ## expectation step
 ## find probability of each cluster for each point
 E.step <- function(X, phi, N) {
@@ -13,7 +15,7 @@ E.step <- function(X, phi, N) {
 
 ## maximization step
 ## given the probability of each cluster for each point
-## find the values of mu, sigma, and alpha that maximize the likelihood
+## find the values of mu, sigma, and alpha that maximizelikelihood
 M.step <- function(X, h, N) {
   covs <- lapply(1:N, function(i) cov.wt(X, h[,i]))
   mu <- lapply(covs, "[[", "center")
@@ -23,10 +25,10 @@ M.step <- function(X, h, N) {
 }
 
 ## log.likelihood
-## given points and parameters, how well does our model fit
-## also gives us a terminating condition 
+## given points and parameters, how well does model fit
+## also gives terminating condition 
 ## stop if don't improve much
-## also used for AIC for model selection (choosing a value of k)
+## used for AIC model selection (choosing a value of k)
 log.like <- function(X, phi, N) {
   probs <- 
     with(phi, do.call(cbind,
@@ -39,19 +41,51 @@ log.like <- function(X, phi, N) {
 ## X = dataset
 ## N = number of clusters
 ## Terminate if log.like @ (t-1) - log.like @ (t) is below a threshold
-run.em <- function(X, N) {
+run.em <- function(X, N, max.iter = 30) {
+  
+  # get initial estimates of means and covariances for each cluster
+  # randomly sample 30 points and build estimates
+  # covs is list of lists
+  # first list is list of first cluster information
+  # etc
   covs <- replicate(N, list(cov.wt(X[sample(nrow(X), 30),])))
+  
+  # list of cluster means
   mu <- lapply(covs, "[[", "center")
+  
+  # list of cluster variances
   sig <- lapply(covs, "[[", "cov")
-  alpha <- rep(1/N, N)
+  
+  # weights
+  alpha <- rep(1 / N, N)
+  
+  # list of lists of parameters for each cluster
   phi <<- list(mu = mu, sig = sig, alpha = alpha)
   
+  # repeat until convergence or maximum iterations reached
   for(i in 1:max.iter) {
+    
     oldphi <- phi
+    # e step
     h <<- E.step(X, phi, N)
+    # m step
     phi <<- M.step(X, h, N)
+    
+    # check for convergence with log-likelihood
     if((log.like(X, phi, N) - log.like(X, oldphi, N)) < 0.01)
       break
   }
-  return(list(phi = phi, aic = 2*3*N - log.like(X, phi, N)))
+  
+  # return cluster information and aic
+  return(list(phi = phi, aic = 2 * 3 * N - log.like(X, phi, N)))
+  
 }
+
+## example run
+two.cluster.data <- 
+  rbind(rmvnorm(n = 1000, mean = c(0, 0), sigma = diag(2) * 10),
+        rmvnorm(n = 1000, mean = c(3, 0),
+                sigma = matrix(data = c(5, 2, 2, 1), nrow = 2, ncol = 2)))
+
+em <- run.em(X = two.cluster.data, N = 2, max.iter = 100)
+km <- kmeans(x = two.cluster.data, centers = 2, iter.max = 30)
