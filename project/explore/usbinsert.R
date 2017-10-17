30a857e2d50633fd6980374cfcec0ec55bede1de
Alexander Durbin <ajd2@vt.edu>
2017-10-17 11:25:31 -0400

    working on usb insertion still

--- project/explore/usbinsert.R
+++ project/explore/usbinsert.R
@@ -4,3 +4,18 @@ test <- combo_filter(usr = usr)
 head(test)
 tmp <- test %>%
   filter(usb != "none")
+
+# going to have to do this without loops after it works
+# for this user it's a 354*886 for loop
+# does this have to be a double for loop though?
+for(the_day in unique(test$day)){
+  for(comp in unique(test$pc)){
+    
+    tmp <- test %>%
+      filter(day == the_day, pc == comp, usb != "none")
+    
+  }
+}
+
+lag(tmp$usb)
+tmp
