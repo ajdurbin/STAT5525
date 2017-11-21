419039796c8b6f0948b8587023c9791ea1a7111a
Alexander Durbin <ajd2@vt.edu>
2017-11-21 09:53:40 -0500

    removed lubridate package for arc usage and substituted functions

--- scripts/employee_summary.R
+++ scripts/employee_summary.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-._ <- c("dplyr", "readr", "plyr", "lubridate", "hms", "glmnet", "caret",
+._ <- c("dplyr", "readr", "plyr", "chron", "hms", "glmnet", "caret",
         "randomForest", "rpart", "stringr", "tm", "wordcloud",
         "animation", "ggplot2", "SnowballC", "topicmodels", "parallel")
 lapply(._, library, character.only = TRUE)
@@ -42,36 +42,39 @@ combo <- function(usr, cur = cur, first = first, file = file, device = device, p
         filter(user == usr) 
     if (nrow(usr_file) != 0) {
         usr_file <- usr_file %>% 
-        mutate(date = lubridate::mdy_hms(date), time = hms::as.hms(date), 
-               day = lubridate::as_date(date), hour = lubridate::hour(date)) %>% 
-            mutate(extension = stringr::str_extract(filename, "[^.]*$")) 
+        mutate(date = strptime(date, "%m/%d/%Y %H:%M:%S"),
+               time = hms::as.hms(date), 
+               day = as.Date(date), hour = chron::hours(date)) %>% 
+        select(-date)
+        mutate(extension = stringr::str_extract(filename, "[^.]*$")) 
     }
         
-    
     
     # usb connects
     usr_device <- device %>%
         filter(user == usr)
     if (nrow(usr_device) != 0){
-        usr_device <- usr_device %>% 
-        mutate(date = lubridate::mdy_hms(date), time = hms::as.hms(date), 
-               day = lubridate::as_date(date), hour = lubridate::hour(date))
+        usr_device <- usr_device %>%
+        mutate(date = strptime(date, "%m/%d/%Y %H:%M:%S"), time = hms::as.hms(date),
+               day = as.Date(date), hour = chron::hours(date)) %>% 
+        select(-date)
     }
-        
+
     # logon
     usr_logon <- logon %>%
-        filter(user == usr) %>% 
-        mutate(date = lubridate::mdy_hms(date), time = hms::as.hms(date), 
-               day = lubridate::as_date(date), hour = lubridate::hour(date))
-    
-    after_hour_logon <- usr_logon %>% 
-        filter(activity == "Logon") %>% 
+        filter(user == usr) %>%
+        mutate(date = strptime(date, "%m/%d/%Y %H:%M:%S"), time = hms::as.hms(date),
+               day = as.Date(date), hour = chron::hours(date)) %>% 
+       select(-date) 
+
+    after_hour_logon <- usr_logon %>%
+        filter(activity == "Logon") %>%
         filter(hour > 0 & hour < 5)
-    
+
     # supervisor information
-    supervisor <- first %>% 
-        filter(user_id == usr) %>% 
-        select(supervisor) %>% 
+    supervisor <- first %>%
+        filter(user_id == usr) %>%
+        select(supervisor) %>%
         as.character()
     if (is.na(supervisor)) {
         supervisor_info <- data.frame(employee_name = "none",
@@ -81,33 +84,34 @@ combo <- function(usr, cur = cur, first = first, file = file, device = device, p
         supervisor_info <- sup %>%
             filter(employee_name == supervisor)
     }
-    
+
     # psychometrics
     usr_psych <- psych %>%
         filter(user_id == usr)
-    
+
     # beginning employees
-    usr_first <- first %>% 
+    usr_first <- first %>%
         filter(user_id == usr)
-    
+
     # # email
     # usr_email <- email %>%
-    #     filter(user == usr) %>% 
-    #     mutate(date = lubridate::mdy_hms(date), time = hms::as.hms(date), 
-    #            day = lubridate::as_date(date), hour = lubridate::hour(date))
+    #     filter(user == usr) %>%
+    #     mutate(date = strptime(date, "%m/%d/%Y %H:%M:%S"), time = hms::as.hms(date),
+    #            day = as.Date(date), hour = chron::hours(date)) %>% 
+    #     select(-date)
     # 
     # # web
     # usr_http <- http %>%
-    #     filter(user == usr) %>% 
-    #     mutate(date = lubridate::mdy_hms(date), time = hms::as.hms(date), 
-    #            day = lubridate::as_date(date), hour = lubridate::hour(date))
+    #     filter(user == usr) %>%
+    #     mutate(date = strptime(date, "%m/%d/%Y %H:%M:%S"), time = hms::as.hms(date),
+    #            day = as.Date(date), hour = chron::hours(date)) %>% 
+    #     select(-date)
     
     row <- data.frame(employee_name = usr_first$employee_name,
                       employee_id = usr,
                       employee_email = usr_first$email,
                       employee_role = usr_first$role,
                       # supervisor information
-                      # supervisor = supervisor,
                       supervisor_name = supervisor_info$employee_name,
                       supervisor_id = supervisor_info$user_id,
                       supervisor_email = supervisor_info$email,
@@ -125,18 +129,18 @@ combo <- function(usr, cur = cur, first = first, file = file, device = device, p
                       total_txt = nrow(usr_file %>% filter(extension == "txt")),
                       total_zip = nrow(usr_file %>% filter(extension == "zip")),
                       # usb connection information
-                      total_connects = 
+                      total_connects =
                           nrow(usr_device %>% filter(activity == "Connect")),
-                      total_disconnects = 
+                      total_disconnects =
                           nrow(usr_device %>% filter(activity == "Disconnect")),
                       total_usb_pc = length(unique(usr_device$pc)),
-                      total_after_hour_connects = 
-                          sum(usr_device$hour <5 & usr_device$hour > 0),
+                      total_after_hour_connects =
+                          sum(usr_device$hour < 5 & usr_device$hour > 0),
                       # logon information
                       total_pc_logon = length(unique(usr_logon$pc)),
                       total_logon =
                           nrow(usr_logon %>% filter(activity == "Logon")),
-                      total_logoff = 
+                      total_logoff =
                           nrow(usr_logon %>% filter(activity == "Logoff")),
                       total_after_hour_logon = nrow(after_hour_logon),
                       # psychometric information
@@ -145,6 +149,7 @@ combo <- function(usr, cur = cur, first = first, file = file, device = device, p
                       E = usr_psych$E,
                       A = usr_psych$A,
                       N = usr_psych$N)
+    
     return(row)
     
 }
